1a4e1d5bc7379d36415b01b215ed52c60cae480f
SPancratz <spancratz@github.com>
2011-02-03 16:37:37 +0000

    Adds "mat_coo/zero.c"

---
+++ mat_coo/zero.c
@@ -0,0 +1,12 @@
+#include "mat_coo.h"
+
+void mat_coo_zero(mat_coo_t A, const mat_ctx_t ctx)
+{
+    long k, u = 2 * sizeof(long) + ctx->size;
+
+    for (k = 0; k < A->length; k++)
+        ctx->clear(A->list + k * u + 2 * sizeof(long));
+
+    A->length = 0;
+}
+
