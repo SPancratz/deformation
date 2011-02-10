fa76290ffee918d62ddb0b5c01a792e6e4889e2e
SPancratz <spancratz@github.com>
2011-02-10 15:30:19 +0000

    Fix a bug in mat_dense_lup_solve and update the documentation

--- mat_dense/lup_solve.c
+++ mat_dense/lup_solve.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 
 #include "mat_dense.h"
+#include "vec.h"
 
 void _mat_dense_lup_solve(char *x, char ** const rows, long m, long n, 
                           const long *pi, const char *b, const mat_ctx_t ctx)
@@ -9,16 +10,9 @@ void _mat_dense_lup_solve(char *x, char ** const rows, long m, long n,
     char *t;
 
     assert(m == n);
+    assert(x != b);
 
-    t = malloc(ctx->size);
-
-    if (!t)
-    {
-        printf("ERROR (_mat_dense_lup_solve).\n\n");
-        abort();
-    }
-
-    ctx->init(t);
+    t = _vec_init(1, ctx);
 
     /* Solve the lower unit-triangular system L y = P b */
 
@@ -34,18 +28,17 @@ void _mat_dense_lup_solve(char *x, char ** const rows, long m, long n,
 
     /* Solve the upper triangular system U x = y */
 
-    for (j = m - 1; j >= 0; j--)
+    for (i = m - 1; i >= 0; i--)
     {
-        for (i = 0; i < j; i++)
+        for (j = i + 1; j < m; j++)
         {
             ctx->mul(t, rows[i] + j * ctx->size, x + j * ctx->size);
             ctx->sub(x + i * ctx->size, x + i * ctx->size, t);
         }
-        ctx->div(x + j * ctx->size, x + j * ctx->size, rows[j] + j * ctx->size);
+        ctx->div(x + i * ctx->size, x + i * ctx->size, rows[i] + i * ctx->size);
     }
 
-    ctx->clear(t);
-    free(t);
+    _vec_clear(t, 1, ctx);
 }
 
 void mat_dense_lup_solve(char *x, const mat_dense_t mat, const long *pi, 
