e1d17402178c47987c02a38bf2584f9c04b0acf7
SPancratz <spancratz@github.com>
2011-07-13 01:03:24 +0200

    Simplify the "gmconnection" example file again

--- examples/gmconnection.c
+++ examples/gmconnection.c
@@ -10,44 +10,14 @@
 #include "mat_csr.h"
 #include "gmconnection.h"
 
-static 
-int _gmc_mat_print(char ** const rows, long m, long n, const ctx_t ctx)
-{
-    long i, j;
-
-    for (i = 0; i < m; i++)
-    {
-        printf("[");
-        for (j = 0; j < n; j++)
-        {
-            ctx->print(ctx, rows[i] + j * ctx->size);
-            if (j != n - 1)
-                printf(" ");
-        }
-        if (i != m - 1)
-            printf("],\n");
-        else
-            printf("]");
-    }
-
-    return 1;
-}
-
-static 
-int gmc_mat_print(const mat_t mat, const ctx_t ctx)
-{
-    return _gmc_mat_print(mat->rows, mat->m, mat->n, ctx);
-}
-
-
-int 
-main(int argc, const char* argv[])
+int main(int argc, const char* argv[])
 {
     ctx_t ctx;
 
     mpoly_t P;
     mat_t M;
     mon_t *rows, *cols;
+    long n;
 
     if (argc != 2)
     {
@@ -58,12 +28,36 @@ main(int argc, const char* argv[])
 
     ctx_init_fmpz_poly_q(ctx);
 
-    mpoly_init(P, 1, ctx);
+    n = atoi(argv[1]) - 1;
+    mpoly_init(P, n, ctx);
     mpoly_set_str(P, argv[1], ctx);
 
     gmc_compute(M, &rows, &cols, P, ctx);
 
-    gmc_mat_print(M, ctx), printf("\n");
+    {
+        long i, j;
+
+        printf("Row index set:\n");
+        for (i = 0; i < M->m; i++)
+        {
+            mon_print_pretty(rows[i], n + 1, NULL);
+            if (i != M->m - 1)
+                printf(", ");
+        }
+        printf("\n");
+        printf("Column index set:\n");
+        for (j = 0; j < M->n; j++)
+        {
+            mon_print_pretty(cols[j], n + 1, NULL);
+            if (j != M->n - 1)
+                printf(", ");
+        }
+        printf("\n");
+    }
+
+    printf("Connection matrix:\n");
+    mat_print(M, ctx);
+    printf("\n");
 
     mpoly_clear(P, ctx);
     mat_clear(M, ctx);
