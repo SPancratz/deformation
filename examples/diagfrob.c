aee9b03b3b76dae13a776c7cc6d76d94a4550668
SPancratz <spancratz@github.com>
2012-05-04 18:53:59 +0100

    New example code for diagfrob

--- examples/diagfrob.c
+++ examples/diagfrob.c
@@ -1,91 +1,60 @@
+/******************************************************************************
+
+    Copyright (C) 2011, 2012 Sebastian Pancratz
+
+******************************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpir.h>
 
 #include "flint.h"
-#include "fmpz.h"
 #include "ulong_extras.h"
-
-#include "mpoly.h"
-#include "mat.h"
 #include "gmconnection.h"
 #include "diagfrob.h"
-#include "deformation.h"
 
-#include "padic_mat.h"
-
-int
-main(void)
+int main(void)
 {
-    int i, result;
-    flint_rand_t state;
-
-    mpoly_t P;
-    mat_t F0;
-    ctx_t ctxFracQt;
-    ctx_t ctxZp;
-    padic_ctx_t pctx;
-    fmpz_t p;
-
-    printf("diagfrob... ");
-    fflush(stdout);
-
-    flint_randinit(state);
-
+    /* Example 1 */
     /*
-        Consider the example 
-
-            W^4 + X^4 + Y^4 + Z^4 + t W X Y Z
+    long n = 3;
+    long d = 4;
+    long N = 374;
+    fmpz_t p = {3L};
+    fmpz a[4] = {1, 1, 1, 1};
      */
-    {
-        char *str = "4  [4 0 0 0] [0 4 0 0] [0 0 4 0] [0 0 0 4] (2  0 1)[1 1 1 1]";
 
-        long b, d, n = atoi(str) - 1;
+    /* Example 2 */
+    long n = 3;
+    long d = 5;
+    long N = 355;
+    fmpz_t p = {2L};
+    fmpz a[4] = {1, 1, 1, 1};
 
-        fmpz_init(p);
-        fmpz_set_ui(p, 3);
+    padic_ctx_t pctx;
+    ctx_t ctx;
 
-        padic_ctx_init(pctx, p, 374, PADIC_VAL_UNIT);
-        ctx_init_padic(ctxZp, pctx);
-        ctx_init_fmpz_poly_q(ctxFracQt);
+    mat_t F;
 
-        mpoly_init(P, n + 1, ctxFracQt);
+    long i, lenB = gmc_basis_size(n, d);
 
-        mpoly_set_str(P, str, ctxFracQt);
-        printf("P = "), mpoly_print(P, ctxFracQt), printf("\n");
+    padic_ctx_init(pctx, p, N, PADIC_VAL_UNIT);
 
-        d = mpoly_degree(P, -1, ctxFracQt);
-        b = gmc_basis_size(n, d);
+    ctx_init_padic(ctx, pctx);
 
-        mat_init(F0, b, b, ctxZp);
+    mat_init(F, lenB, lenB, ctx);
+    diagfrob(F, a, n, d, ctx, 1);
 
-        /* Find F(0) */
-        {
-            fmpz * a = _fmpz_vec_init(n + 1);
+    mat_print(F, ctx);
+    printf("\n\n");
 
-            mpoly_diagonal_fibre(a, P, ctxFracQt);
+    /* Clean-up */
 
-            diagfrob(F0, a, n, d, ctxZp);
+    fmpz_clear(p);
+    mat_clear(F, ctx);
+    padic_ctx_clear(pctx);
+    ctx_clear(ctx);
 
-            printf("a = {"), _fmpz_vec_print(a, n + 1), printf("}\n");
-
-            _fmpz_vec_clear(a, n + 1);
-        }
-
-        printf("Matrix F(0):\n");
-        mat_print(F0, ctxZp);
-        printf("\n");
-
-        mat_clear(F0, ctxZp);
-        mpoly_clear(P, ctxFracQt);
-        padic_ctx_clear(pctx);
-        ctx_clear(ctxZp);
-        ctx_clear(ctxFracQt);
-    }
-
-    flint_randclear(state);
-    _fmpz_cleanup();
-    printf("PASS\n");
     return EXIT_SUCCESS;
 }
 
