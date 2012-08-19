b9a9e13d5038bc6dd6d24449fc110a47dcae0900
Sebastian Pancratz <sebastian.pancratz@gmail.com>
2012-08-19 12:23:42 +0200

    Explicitly adds two examples from the diagfrob section.

---
+++ examples/diagfrob-02.c
@@ -0,0 +1,46 @@
+/******************************************************************************
+
+    Copyright (C) 2011, 2012 Sebastian Pancratz
+
+******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpir.h>
+
+#include "flint.h"
+#include "ulong_extras.h"
+#include "gmconnection.h"
+#include "diagfrob.h"
+
+int main(void)
+{
+    /* Example 2 */
+    long n = 3;
+    long d = 5;
+    long N = 80;
+    fmpz_t p = {2L};
+    fmpz a[4] = {1, 1, 1, 1};
+
+    padic_ctx_t pctx;
+
+    padic_mat_t F;
+
+    long i, lenB = gmc_basis_size(n, d);
+
+    padic_ctx_init(pctx, p, N, PADIC_VAL_UNIT);
+
+    padic_mat_init(F, lenB, lenB);
+    diagfrob(F, a, n, d, pctx, 1);
+
+    padic_mat_print_pretty(F, pctx);
+    printf("\n\n");
+
+    /* Clean-up */
+    fmpz_clear(p);
+    padic_mat_clear(F);
+    padic_ctx_clear(pctx);
+
+    return EXIT_SUCCESS;
+}
+
